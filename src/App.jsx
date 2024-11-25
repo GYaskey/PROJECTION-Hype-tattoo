dbc4be0c3c25ef8bf07a49da320d210309c52aca
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-11-25 21:28:18 +0200

    background

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <div className="heroAudienceWrapper">
         <Header />
-        <Hero />
+        <Hero className="hero" />
         <Audience />
       </div>
       <Packages />
